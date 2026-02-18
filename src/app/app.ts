9f772ca2d772fa71c7de35a6743ce53a5302ec38
Jetro <jetropapa@gmail.com>
2026-02-18 16:28:21 +0800

    init

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,25 @@
 import { Component, signal } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, ReactiveFormsModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('internship-day-1-task');
+
+  bibleForm = new FormGroup({
+    verse: new FormControl(''),
+    version: new FormControl(''),
+    content: new FormControl(''),
+  });
+
+  submitForm(event: Event) {
+    event.preventDefault();
+    console.log('Form Submited!');
+    console.log(this.bibleForm);
+  }
 }
