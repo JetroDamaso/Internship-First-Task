9bfb96404c23e51eb6c7a4d33e66e6fb3fa0874c
Jetro <jetropapa@gmail.com>
2026-02-19 00:56:16 +0800

    added POST, ui impovements, converted to signal

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,6 +4,7 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 import { LucideAngularModule, Plus, RotateCcw } from 'lucide-angular';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -20,21 +21,27 @@ export class App implements OnInit {
 
   //Forms
   bibleForm = new FormGroup({
-    verse: new FormControl(''),
-    version: new FormControl(''),
-    content: new FormControl(''),
+    verse: new FormControl(),
+    version: new FormControl(),
+    content: new FormControl(),
   });
 
   submitForm(event: Event) {
     event.preventDefault();
-    console.log('Form Submited!');
+
+    if (this.bibleForm.invalid) {
+      console.log('Form is invalid');
+      return;
+    }
+
+    this.addVerse();
     console.log(this.bibleForm);
   }
 
   //RESTful APIs
   http = inject(HttpClient);
 
-  verses: any[] = [];
+  verses = signal<any[]>([]);
 
   //Run this on initialize
   ngOnInit(): void {
@@ -44,8 +51,26 @@ export class App implements OnInit {
   getAllVerses() {
     console.log('Get Verses Clicked!');
     this.http.get('http://localhost:8080/bible_verses').subscribe((data: any) => {
-      this.verses = data;
+      this.verses.set(data);
       console.log(data);
+    });
+  }
+
+  addVerse() {
+    const formData = this.bibleForm.value;
+
+    this.http.post('http://localhost:8080/bible_verses/create', formData).subscribe({
+      next: (res) => {
+        console.log('Successfully created verse:', res);
+
+        this.getAllVerses();
+
+        this.bibleForm.reset();
+      },
+      error: (error) => {
+        alert('Error creating verse!');
+        console.error('Error creating verse:', error);
+      },
     });
   }
 }
