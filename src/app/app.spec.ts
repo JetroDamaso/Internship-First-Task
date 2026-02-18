35de182803369c894baaeaeb6bedd23ae687449e
Jetro <jetropapa@gmail.com>
2026-02-18 16:52:53 +0800

    <h1> to <p>

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, internship-day-1-task');
+    expect(compiled.querySelector('p')?.textContent).toContain('This is where we see our data');
   });
 });
